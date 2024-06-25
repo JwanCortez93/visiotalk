0f5d5546584d6714cf2f100100fba431ff69f8bb
JwanCortez93 <jwancortez@gmail.com>
2024-06-25 09:04:44 -0300

    NavBar and MobileNav init

---
+++ src/app/(root)/(home)/_components/MobileNav.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Menu />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-foreground">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src={"/logo/svg/logo-no-background.svg"}
+              width={200}
+              height={200}
+              alt="VisioTalk Logo"
+            />
+          </Link>
+          <div className="flex flex-col h-[calc(100vh-72px)] justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex flex-col h-full pt-16 gap-6 text-secondary-foreground">
+                {sidebarLinks.map((link) => {
+                  const isActive =
+                    pathname === link.route ||
+                    pathname.startsWith(`${link.route}/`);
+                  return (
+                    <SheetClose asChild key={link.route}>
+                      <Link
+                        href={link.route}
+                        key={link.label}
+                        className={cn(
+                          "flex gap-4 items-center p-4 rounded-lg w-full",
+                          { "bg-primary": isActive }
+                        )}
+                      >
+                        {link.icon}
+                        <p className=" font-semibold ">{link.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
--- src/app/(root)/(home)/_components/NavBar.tsx
+++ src/app/(root)/(home)/_components/NavBar.tsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full text-secondary-foreground bg-primary px-6 py-4 lg:px-10">
+      <Link href="/" className="hidden items-center gap-1 lg:flex">
+        <Image
+          src={"/logo/svg/logo-no-background.svg"}
+          width={200}
+          height={200}
+          alt="VisioTalk Logo"
+        />
+      </Link>
+      <Link href="/" className="lg:hidden flex items-center gap-1">
+        <Image
+          src={"/logo/png/visiotalk-favicon-color.png"}
+          width={50}
+          height={50}
+          alt="VisioTalk Logo"
+        />
+      </Link>
+      <div className="flex-between gap-5 sm:hidden">
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
